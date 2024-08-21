bb5656723949936fa1ce18681ce655f93abdf325
Paul DeVincentz <rapps2002@hotmail.com>
2024-08-21 14:19:24 -0400

    creating initial layout and header (static)

---
+++ app-ggb/app/components/header.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useState } from "react"
+import Link from "next/link"
+import './header.css'
+
+export default function Header() {
+    const [isShowNavDropdown, setIsShowNavDropdown] = useState<boolean>(false)
+
+    const onToggleNav = () => {
+        setIsShowNavDropdown(!isShowNavDropdown)
+    }
+    const onReset = () => {
+        if (confirm("Reset will delete all saved gameboards and reload the page resetting the current config. Continue?")) {
+            localStorage.clear();
+            /*this.gameboardService.doLoad();
+            this.gameboardService.currentBoard.doRandomizeRows(true);
+            this.router.navigate(["/"]);*/
+        }
+    }
+
+    return (
+        <div id="header-bar">
+            <Link href="/">
+                <img id="gg-logo" src="/assets/images/gg_logo.png" />
+                <img id="bingo-logo" src="/assets/images/bingo_logo.png" />
+            </Link>
+            <span id="config-name" className="hidden"></span>
+            <span id="header-menu" onClick={onToggleNav}>
+                <img id="btn-menu" className="clickable" src="/assets/images/icons8-menu-vertical-50.png" />
+            </span>
+            {!isShowNavDropdown ? <></> : 
+                <div id="header-menu-popup">
+                    <ul className="ul-nostyle">
+                        <li id="btn-menu-back" onClick={onToggleNav}><img src="/assets/images/icons8-menu-vertical-50.png" /></li>
+                        <li id="btn-config"><Link href="/config" onClick={onToggleNav}><img src="/assets/images/icons8-settings-50.svg" />Config</Link></li>
+                        <li id="btn-saved"><Link href="/saved-boards" onClick={onToggleNav}><img src="/assets/images/icons8-playlist-50.png" />Saved</Link></li>
+                        <li id="btn-share"><Link href="/share" onClick={onToggleNav}><img src="/assets/images/icons8-share-30.svg" />Share</Link></li>
+                        <li id="btn-reset"><Link href="#" onClick={() => {onReset(); onToggleNav()}}><img src="/assets/images/icons8-reset-50.png" />Reset</Link></li>
+                        <li id="btn-about"><Link href="/about" onClick={onToggleNav}><img src="/assets/images/icons8-info-50.svg" />About</Link></li>
+                    </ul>
+                </div>
+            }
+        </div>
+    )
+}
--- app-ggb/app/layout.tsx
+++ app-ggb/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./layout.css";
+import Header from "./components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div id='ggb'>
+          <div id="background">
+              <div id="background-left"></div>
+              <div id="background-right"></div>
+          </div>
+          <div id="content">
+              <Header />
+              {children}
+              <div id="dialogs">
+                  <div id="dlg" className="dialog"></div>
+              </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
