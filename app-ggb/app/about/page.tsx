e838cb0c46b104148a04b27bb8b195437bae3dad
Paul DeVincentz <rapps2002@hotmail.com>
2024-08-22 10:28:50 -0400

    conversion of about

---
+++ app-ggb/app/about/page.tsx
@@ -0,0 +1,11 @@
+import "./page.css"
+
+export default function About() {
+    return (
+        <div id="about-wrapper" className="content-body">
+            <div>About</div>
+            <div>Background images are official <a target="_blank" href="https://merch.gamegrumps.com/">Game Grumps</a> posters by <a target="_blank" href="https://x.com/K1ttensocks">@K1ttensocks</a>.</div>
+            <div><a target="_blank" href="https://icons8.com/icons/set/menu">Menu</a>, <a target="_blank" href="https://icons8.com/icons/set/delete-sign">Delete</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a>.</div>
+        </div>
+    )
+}
